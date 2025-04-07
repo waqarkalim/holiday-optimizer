5cf172c731e07bb66107dcd45ae257616d7d27d1
Waqar Bin Kalim <waqaar.199@gmail.com>
2025-04-06 20:05:43 -0400

    Add XFollowPopup component for Twitter follow prompt
    - Introduce XFollowPopup to encourage users to follow on Twitter with a customizable delay.
    - Update Providers component to include the new XFollowPopup and adjust ThemeProvider settings.
    - Refactor Toaster component usage for improved functionality.

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -5,7 +5,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ThemeProvider } from '@/components/ThemeProvider';
 import { TooltipProvider } from '@/components/ui/tooltip';
-import { Toaster } from 'sonner';
+import { Toaster } from '@/components/ui/sonner';
+import XFollowPopup from '@/components/XFollowPopup';
 
 // Create a client
 export const queryClient = new QueryClient({
@@ -18,12 +19,14 @@ export const queryClient = new QueryClient({
   },
 });
 
+
 export const Providers = ({ children }: { children: React.ReactNode }) => (
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider attribute="class" defaultTheme="light" disableTransitionOnChange>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
       <TooltipProvider>
         {children}
-        <Toaster richColors closeButton position="bottom-right" />
+        <Toaster position="bottom-right" />
+        <XFollowPopup twitterHandle="waqar_kalim"  />
       </TooltipProvider>
     </ThemeProvider>
     {process.env.NODE_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false} />}
